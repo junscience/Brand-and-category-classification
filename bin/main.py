7185488d047d2e0ca25d8ee65514b26f8f5d0d16
junscience <popenko.antony@gmail.com>
2024-03-03 19:50:28 +0200

    added needed files to git'

---
+++ bin/main.py
@@ -0,0 +1,4 @@
+#!"/home/user/Files/Data Science/Embeddings_remastered/Embeddings/bin/python"
+# EASY-INSTALL-SCRIPT: 'brand-category-classification==0.0.1','main.py'
+__requires__ = 'brand-category-classification==0.0.1'
+__import__('pkg_resources').run_script('brand-category-classification==0.0.1', 'main.py')
